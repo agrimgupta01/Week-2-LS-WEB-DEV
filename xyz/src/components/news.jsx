7f7d0af30ecb246192b5f7cf50f684f0cd62d124
agrimgupta01 <agrim921@gmail.com>
2022-07-25 01:50:14 +0530

    2nd commit

--- xyz/src/components/news.jsx
+++ xyz/src/components/news.jsx
@@ -3,24 +3,31 @@ function News() {
     return (
         <form>
             <div class="row mb-3">
-                <label for="inputEmail3" class="col-sm-2 col-form-label">Email</label>
+                <label for="inputEmail3" class="col-sm-2 col-form-label">Name</label>
                 <div class="col-sm-10">
                     <input type="email" class="form-control" id="inputEmail3" />
                 </div>
             </div>
             <div class="row mb-3">
-                <label for="inputPassword3" class="col-sm-2 col-form-label">Password</label>
+                <label for="inputPassword3" class="col-sm-2 col-form-label">Email</label>
                 <div class="col-sm-10">
-                    <input type="password" class="form-control" id="inputPassword3" />
+                    <input type="text" class="form-control" id="inputPassword3" />
                 </div>
             </div>
             <div class="row mb-3">
-                <label for="inputPassword3" class="col-sm-2 col-form-label">Password</label>
+                <label for="inputPassword3" class="col-sm-2 col-form-label">Room-Number</label>
                 <div class="col-sm-10">
-                    <input type="password" class="form-control" id="inputPassword3" />
+                    <input type="number" class="form-control" id="inputPassword3" />
                 </div>
             </div>
-            <button type="submit" class="btn btn-primary">Sign in</button>
+            <div class="row mb-3">
+                <label for="inputPassword3" class="col-sm-2 col-form-label">Phone-Number</label>
+                <div class="col-sm-10">
+                    <input type="number" class="form-control" id="inputPassword3" />
+                </div>
+            </div>
+
+            <button type="submit" class="btn btn-primary">Subscribe</button>
         </form>
     )
 }
--- xyz/src/components/review.jsx
+++ xyz/src/components/review.jsx
@@ -4,12 +4,12 @@ function Review() {
         <div class="card mb-3" >
             <div class="row g-0">
                 <div class="col-md-2">
-                    <img src="#" width="130" height="200" />
+                    <img src="https://source.unsplash.com/200x180/?person" width="180" height="200" />
                 </div>
                 <div class="col-md-10">
                     <div class="card-body">
-                        <h5 class="card-title">Card title</h5>
-                        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                        <h5 class="card-title">Review</h5>
+                        <p class="card-text">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolore, exercitationem! Culpa nihil in facere vitae molestiae? Laboriosam dolore laborum rem cum adipisci temporibus repellendus, vitae quaerat nihil minus, magnam esse, inventore molestias vero. Voluptatem, quaerat ut laborum id fugit nam, officia voluptate labore itaque, qui odio esse quibusdam sint unde!</p>
                         <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
                     </div>
                 </div>
